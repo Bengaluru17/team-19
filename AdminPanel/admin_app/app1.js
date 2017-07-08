efe4790626cf20dccfd1189ea60a0855759190dc
FaizanKhan <faizankh29@gmail.com>
2017-07-08 23:22:49 +0530

    Render data to frontend

--- AdminPanel/admin_app/app1.js
+++ AdminPanel/admin_app/app1.js
@@ -23,6 +23,7 @@ query.on("row", function (row, result) {
     result.addRow(row);
 });
 query.on("end", function (result) {
+    
     console.log(JSON.stringify(result.rows, null, "    "));
     client.end();
 });
