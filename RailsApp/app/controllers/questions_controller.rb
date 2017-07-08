587ad08da958675f713c8c2e62aba0f8b7b5b80d
Abishek Aditya <abishekaditya@outlook.com>
2017-07-08 23:00:54 +0530

    questions controller

--- RailsApp/app/controllers/questions_controller.rb
+++ RailsApp/app/controllers/questions_controller.rb
@@ -9,10 +9,10 @@ class QuestionsController < ApplicationController
 		Question.where(group: 1, subgroup: 1, level: 5).each do |f|
 			puts f.answer
 			puts params[f.question.to_s]
+			puts @ctr
 			byebug
 			(@ctr += 1) if params[f.question.to_s] == f.answer.to_s
 		end
-		
 		Score.create(:group => 1, :subgroup => 1, :score => @ctr, :student_id => 1)
 		redirect_to(root_path)
 	end
