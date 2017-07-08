de1c7a0bb0dd31432d815d8bf32150ef5ba1f689
Abishek Aditya <abishekaditya@outlook.com>
2017-07-08 17:43:16 +0530

    md5 login for admins

--- RailsApp/app/models/student.rb
+++ RailsApp/app/models/student.rb
@@ -1,6 +1,6 @@
 class Student < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
-  devise :database_authenticatable, :registerable,
+  devise :database_authenticatable, :registerable, :encryptable,
          :recoverable, :rememberable, :trackable, :validatable
 end
