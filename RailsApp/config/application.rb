d04673d6f651e3f2ab39f6ca5c94d80fa857f041
Abishek Aditya <abishekaditya@outlook.com>
2017-07-09 01:32:13 +0530

    no precompilation in dev

--- RailsApp/config/application.rb
+++ RailsApp/config/application.rb
@@ -7,8 +7,6 @@ require 'rails/all'
 Bundler.require(*Rails.groups)
 module RailsApp
   class Application < Rails::Application
-    config.assets.initialize_on_precompile = false
-    config.serve_static_assets = true
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
