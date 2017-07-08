3815c7f559e4fe7997735595212e84eae80d352e
Abishek Aditya <abishekaditya@outlook.com>
2017-07-09 02:25:49 +0530

    assets

--- RailsApp/config/initializers/assets.rb
+++ RailsApp/config/initializers/assets.rb
@@ -4,6 +4,7 @@
 Rails.application.config.assets.version = '1.0'
 Rails.application.config.assets.precompile += %w( bootstrap.css )
 Rails.application.config.assets.precompile += %w( jquery.dataTables.min.css )
+Rails.application.config.assets.precompile += %w( d3.js )
 # Add additional assets to the asset load path
 # Rails.application.config.assets.paths << Emoji.images_path
 
